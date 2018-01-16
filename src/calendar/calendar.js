b2856fbdc7f348bdae8fd139d76f09e6d664d887
Sarfraz <sarfraztmm@gmail.com>
2018-01-16 19:50:08 +0300

    ionic calendar srk

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,41 +1,41 @@
-var __decorate = (this && this.__decorate) || function(decorators, target, key, desc) {
-    var c = arguments.length,
-        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
-        d;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else
-        for (var i = decorators.length - 1; i >= 0; i--)
-            if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function(k, v) {
+var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import * as moment from 'moment';
 import * as _ from "lodash";
-var Calendar = (function() {
+var Calendar = /** @class */ (function () {
     function Calendar() {
         this.onDaySelect = new EventEmitter();
-        this.dateArray = [];
-        this.weekArray = [];
-        this.lastSelect = 0;
-
+        this.onMonthSelect = new EventEmitter();
+        this.events = [];
+        this.dateArray = []; // Array for all the days of the month
+        this.weekArray = []; // Array for each row of the calendar
+        this.lastSelect = 0; // Record the last clicked location
         this.weekHead = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
         this.currentYear = moment().year();
         this.currentMonth = moment().month();
         this.currentDate = moment().date();
         this.currentDay = moment().day();
     }
-    Calendar.prototype.ngOnInit = function() {
+    Calendar.prototype.ngOnChanges = function () {
+        this.createMonth(this.displayYear, this.displayMonth);
+    };
+    Calendar.prototype.ngAfterViewInit = function () {
         this.today();
     };
-
-    Calendar.prototype.today = function() {
+    // Jump to today
+    Calendar.prototype.today = function () {
         this.displayYear = this.currentYear;
         this.displayMonth = this.currentMonth;
         this.createMonth(this.currentYear, this.currentMonth);
-
+        // Mark today as a selection
         var todayIndex = _.findIndex(this.dateArray, {
             year: this.currentYear,
             month: this.currentMonth,
@@ -46,25 +46,40 @@ var Calendar = (function() {
         this.dateArray[todayIndex].isSelect = true;
         this.onDaySelect.emit(this.dateArray[todayIndex]);
     };
-    Calendar.prototype.createMonth = function(year, month) {
-        this.dateArray = [];
-        this.weekArray = [];
+    Calendar.prototype.isInEvents = function (year, month, date) {
+        var i = 0, len = this.events.length;
+        for (; i < len; i++) {
+            if (this.events[i].year == year && this.events[i].month == month && this.events[i].date == date) {
+                return true;
+            }
+        }
+        return false;
+    };
+    Calendar.prototype.createMonth = function (year, month) {
+        this.dateArray = []; // Clear last month's data
+        this.weekArray = []; // Clear week data
         var firstDay;
-        var preMonthDays;
-        var monthDays;
+        // The day of the week on the first day of the current month of
+        // selection determines how many days to take out last month. Sunday
+        // does not show last month, Monday shows the previous month, Tuesday
+        // shows the last two days
+        var preMonthDays; // The number of days for the previous month
+        var monthDays; // The number of days for the month
         var weekDays = [];
         firstDay = moment({ year: year, month: month, date: 1 }).day();
-
+        // The number of days last month
         if (month === 0) {
             preMonthDays = moment({ year: year - 1, month: 11 }).daysInMonth();
-        } else {
+        }
+        else {
             preMonthDays = moment({ year: year, month: month - 1 }).daysInMonth();
         }
-
+        // The number of days this month
         monthDays = moment({ year: year, month: month }).daysInMonth();
-
+        // PREVIOUS MONTH
+        // Add the last few days of the previous month to the array
         if (firstDay !== 7) {
-            var lastMonthStart = preMonthDays - firstDay + 1;
+            var lastMonthStart = preMonthDays - firstDay + 1; // From the last few months start
             for (var i = 0; i < firstDay; i++) {
                 if (month === 0) {
                     this.dateArray.push({
@@ -74,8 +89,10 @@ var Calendar = (function() {
                         isThisMonth: false,
                         isToday: false,
                         isSelect: false,
+                        hasEvent: (this.isInEvents(year, 11, lastMonthStart + i)) ? true : false,
                     });
-                } else {
+                }
+                else {
                     this.dateArray.push({
                         year: year,
                         month: month - 1,
@@ -83,11 +100,12 @@ var Calendar = (function() {
                         isThisMonth: false,
                         isToday: false,
                         isSelect: false,
+                        hasEvent: (this.isInEvents(year, month - 1, lastMonthStart + i)) ? true : false,
                     });
                 }
             }
         }
-
+        // Add the numeral for this month to the array
         for (var i = 0; i < monthDays; i++) {
             this.dateArray.push({
                 year: year,
@@ -96,6 +114,7 @@ var Calendar = (function() {
                 isThisMonth: true,
                 isToday: false,
                 isSelect: false,
+                hasEvent: (this.isInEvents(year, month, i + 1)) ? true : false,
             });
         }
         if (this.currentYear === year && this.currentMonth === month) {
@@ -107,7 +126,7 @@ var Calendar = (function() {
             });
             this.dateArray[todayIndex].isToday = true;
         }
-
+        // Add the number of days next month to the array, with some months showing 6 weeks and some months showing 5 weeks
         if (this.dateArray.length % 7 !== 0) {
             var nextMonthAdd = 7 - this.dateArray.length % 7;
             for (var i = 0; i < nextMonthAdd; i++) {
@@ -119,8 +138,10 @@ var Calendar = (function() {
                         isThisMonth: false,
                         isToday: false,
                         isSelect: false,
+                        hasEvent: (this.isInEvents(year, 0, i + 1)) ? true : false,
                     });
-                } else {
+                }
+                else {
                     this.dateArray.push({
                         year: year,
                         month: month + 1,
@@ -128,11 +149,13 @@ var Calendar = (function() {
                         isThisMonth: false,
                         isToday: false,
                         isSelect: false,
+                        hasEvent: (this.isInEvents(year, month + 1, i + 1)) ? true : false,
                     });
                 }
             }
         }
-
+        // All date data is now added to the dateArray array
+        // Insert the date data into the new array every seven days
         for (var i = 0; i < this.dateArray.length / 7; i++) {
             for (var j = 0; j < 7; j++) {
                 weekDays.push(this.dateArray[i * 7 + j]);
@@ -141,47 +164,65 @@ var Calendar = (function() {
             weekDays = [];
         }
     };
-    Calendar.prototype.back = function() {
-
+    Calendar.prototype.back = function () {
+        // Decrementing the year if necessary
         if (this.displayMonth === 0) {
             this.displayYear--;
             this.displayMonth = 11;
-        } else {
+        }
+        else {
             this.displayMonth--;
         }
+        this.onMonthSelect.emit({
+            'year': this.displayYear,
+            'month': this.displayMonth
+        });
         this.createMonth(this.displayYear, this.displayMonth);
     };
-    Calendar.prototype.forward = function() {
-
+    Calendar.prototype.forward = function () {
+        // Incrementing the year if necessary
         if (this.displayMonth === 11) {
             this.displayYear++;
             this.displayMonth = 0;
-        } else {
+        }
+        else {
             this.displayMonth++;
         }
+        this.onMonthSelect.emit({
+            'year': this.displayYear,
+            'month': this.displayMonth
+        });
         this.createMonth(this.displayYear, this.displayMonth);
     };
-
-    Calendar.prototype.daySelect = function(day, i, j) {
-
+    // Select a day, click event
+    Calendar.prototype.daySelect = function (day, i, j) {
+        // First clear the last click status
         this.dateArray[this.lastSelect].isSelect = false;
-
+        // Store this clicked status
         this.lastSelect = i * 7 + j;
         this.dateArray[i * 7 + j].isSelect = true;
         this.onDaySelect.emit(day);
     };
+    __decorate([
+        Output(),
+        __metadata("design:type", Object)
+    ], Calendar.prototype, "onDaySelect", void 0);
+    __decorate([
+        Output(),
+        __metadata("design:type", Object)
+    ], Calendar.prototype, "onMonthSelect", void 0);
+    __decorate([
+        Input(),
+        __metadata("design:type", Array)
+    ], Calendar.prototype, "events", void 0);
+    Calendar = __decorate([
+        Component({
+            selector: 'ion-calendar',
+            template: "\n    <ion-grid>\n        <ion-row justify-content-center>\n            <ion-col col-2 col-auto (click)=\"back()\" text-left>\n                <ion-icon ios=\"ios-arrow-back\" md=\"md-arrow-back\"></ion-icon>\n            </ion-col>\n            <ion-col col-8 col-auto text-center>\n                <div>{{displayMonth + 1 | monthName}} - {{displayYear}}</div>\n            </ion-col>\n            <ion-col col-2 col-auto (click)=\"forward()\" text-right>\n                <ion-icon ios=\"ios-arrow-forward\" md=\"md-arrow-forward\"></ion-icon>\n            </ion-col>\n        </ion-row>\n\n        <ion-row>\n            <ion-col class=\"center calendar-header-col\" *ngFor=\"let head of weekHead\">{{head}}</ion-col>\n        </ion-row>\n\n        <ion-row class=\"calendar-row\" *ngFor=\"let week of weekArray;let i = index\">\n            <ion-col class=\"center calendar-col\" (click)=\"daySelect(day,i,j)\"\n            *ngFor=\"let day of week;let j = index\"\n            [ngClass]=\"[day.isThisMonth?'this-month':'not-this-month',day.isToday?'today':'',day.isSelect?'select':'']\">\n                {{day.date}}\n                <span class=\"eventBlip\" *ngIf=\"day.hasEvent\"></span>\n            </ion-col>\n        </ion-row>\n\n    </ion-grid>\n"
+        }),
+        __metadata("design:paramtypes", [])
+    ], Calendar);
     return Calendar;
 }());
-__decorate([
-    Output(),
-    __metadata("design:type", Object)
-], Calendar.prototype, "onDaySelect", void 0);
-Calendar = __decorate([
-    Component({
-        selector: 'ion-calendar',
-        template: "\n    <ion-grid>\n        <ion-row justify-content-center>\n            <ion-col col-2 col-auto (click)=\"back()\" text-left>\n                <ion-icon ios=\"ios-arrow-back\" md=\"md-arrow-back\"></ion-icon>\n            </ion-col>\n            <ion-col col-8 col-auto text-center>\n                <div>{{displayMonth + 1}}  {{displayYear}} </div>\n            </ion-col>\n            <ion-col col-2 col-auto (click)=\"forward()\" text-right>\n                <ion-icon ios=\"ios-arrow-forward\" md=\"md-arrow-forward\"></ion-icon>\n            </ion-col>\n        </ion-row>\n\n        <ion-row>\n            <ion-col class=\"center calendar-header-col\" *ngFor=\"let head of weekHead\">{{head}}</ion-col>\n        </ion-row>\n\n        <ion-row class=\"calendar-row\" *ngFor=\"let week of weekArray;let i = index\">\n            <ion-col class=\"center calendar-col\" (click)=\"daySelect(day,i,j)\" \n            *ngFor=\"let day of week;let j = index\" \n            [ngClass]=\"[day.isThisMonth?'this-month':'not-this-month',day.isToday?'today':'',day.isSelect?'select':'']\">\n                {{day.date}}\n            </ion-col>\n        </ion-row>\n\n    </ion-grid>\n"
-    }),
-    __metadata("design:paramtypes", [])
-], Calendar);
 export { Calendar };
 //# sourceMappingURL=calendar.js.map
--- src/calendar/calendar.ts
+++ src/calendar/calendar.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import * as moment from 'moment';
 import * as _ from "lodash";
 
@@ -11,25 +11,23 @@ import * as _ from "lodash";
                 <ion-icon ios="ios-arrow-back" md="md-arrow-back"></ion-icon>
             </ion-col>
             <ion-col col-8 col-auto text-center>
-                <div> {{displayMonth + 1}} {{displayYear}}</div>
+                <div>{{displayMonth + 1 | monthName}} - {{displayYear}}</div>
             </ion-col>
             <ion-col col-2 col-auto (click)="forward()" text-right>
                 <ion-icon ios="ios-arrow-forward" md="md-arrow-forward"></ion-icon>
             </ion-col>
         </ion-row>
-
         <ion-row>
             <ion-col class="center calendar-header-col" *ngFor="let head of weekHead">{{head}}</ion-col>
         </ion-row>
-
         <ion-row class="calendar-row" *ngFor="let week of weekArray;let i = index">
-            <ion-col class="center calendar-col" (click)="daySelect(day,i,j)" 
-            *ngFor="let day of week;let j = index" 
+            <ion-col class="center calendar-col" (click)="daySelect(day,i,j)"
+            *ngFor="let day of week;let j = index"
             [ngClass]="[day.isThisMonth?'this-month':'not-this-month',day.isToday?'today':'',day.isSelect?'select':'']">
                 {{day.date}}
+                <span class="eventBlip" *ngIf="day.hasEvent"></span>
             </ion-col>
         </ion-row>
-
     </ion-grid>
 `
 })
@@ -37,45 +35,41 @@ import * as _ from "lodash";
 export class Calendar {
 
     @Output() onDaySelect = new EventEmitter<dateObj>();
+    @Output() onMonthSelect = new EventEmitter<any>();
+    @Input() events: Array<singularDate> = [];
 
-    currentYear: number;
+    currentYear: number = moment().year();
+    currentMonth: number = moment().month();
+    currentDate: number = moment().date();
+    currentDay: number = moment().day();
 
-    currentMonth: number;
+    displayYear: number = moment().year();
+    displayMonth: number = moment().month();
 
-    currentDate: number;
-
-    currentDay: number;
-
-    displayYear: number;
-
-    displayMonth: number;
-
-    dateArray: Array<dateObj> = []; 
-
-    weekArray = [];
-
-    lastSelect: number = 0; 
-    
+    dateArray: Array<dateObj> = []; // Array for all the days of the month
+    weekArray = []; // Array for each row of the calendar
+    lastSelect: number = 0; // Record the last clicked location
     weekHead: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-
     constructor() {
-        this.currentYear = moment().year();
-        this.currentMonth = moment().month();
-        this.currentDate = moment().date();
-        this.currentDay = moment().day();
+      this.today();
+      this.createMonth(this.displayYear, this.displayMonth);
     }
 
-    ngOnInit() {
-        this.today()
+    ngOnChanges() {
+      this.createMonth(this.displayYear, this.displayMonth);
     }
 
+    ngAfterContentInit() {
+    }
+
+    // Jump to today
     today() {
         this.displayYear = this.currentYear;
         this.displayMonth = this.currentMonth;
         this.createMonth(this.currentYear, this.currentMonth);
 
-       
+        // Mark today as a selection
         let todayIndex = _.findIndex(this.dateArray, {
             year: this.currentYear,
             month: this.currentMonth,
@@ -88,27 +82,42 @@ export class Calendar {
         this.onDaySelect.emit(this.dateArray[todayIndex]);
     }
 
+    isInEvents(year, month, date) {
+      var i=0, len=this.events.length;
+      for (; i<len; i++) {
+        if (this.events[i].year == year && this.events[i].month == month && this.events[i].date == date) {
+          return true;
+        }
+      }
+      return false;
+    }
+
     createMonth(year: number, month: number) {
-        this.dateArray = [];
-        this.weekArray = [];
+        this.dateArray = []; // Clear last month's data
+        this.weekArray = []; // Clear week data
         let firstDay;
-        let preMonthDays;
-        let monthDays;
+        // The day of the week on the first day of the current month of
+        // selection determines how many days to take out last month. Sunday
+        // does not show last month, Monday shows the previous month, Tuesday
+        // shows the last two days
+        let preMonthDays; // The number of days for the previous month
+        let monthDays; // The number of days for the month
         let weekDays: Array<dateObj> = [];
 
         firstDay = moment({ year: year, month: month, date: 1 }).day();
-       
+        // The number of days last month
         if (month === 0) {
             preMonthDays = moment({ year: year - 1, month: 11 }).daysInMonth();
         } else {
             preMonthDays = moment({ year: year, month: month - 1 }).daysInMonth();
         }
-        
+        // The number of days this month
         monthDays = moment({ year: year, month: month }).daysInMonth();
 
-        
-        if (firstDay !== 7) {
-            let lastMonthStart = preMonthDays - firstDay + 1;
+        // PREVIOUS MONTH
+        // Add the last few days of the previous month to the array
+        if (firstDay !== 7) { // Sunday doesn't need to be shown for the previous month
+            let lastMonthStart = preMonthDays - firstDay + 1; // From the last few months start
             for (let i = 0; i < firstDay; i++) {
                 if (month === 0) {
                     this.dateArray.push({
@@ -118,6 +127,7 @@ export class Calendar {
                         isThisMonth: false,
                         isToday: false,
                         isSelect: false,
+                        hasEvent: (this.isInEvents(year, 11, lastMonthStart+i)) ? true : false,
                     })
                 } else {
                     this.dateArray.push({
@@ -127,13 +137,14 @@ export class Calendar {
                         isThisMonth: false,
                         isToday: false,
                         isSelect: false,
+                        hasEvent: (this.isInEvents(year, month-1, lastMonthStart+i)) ? true : false,
                     })
                 }
 
             }
         }
 
-        
+        // Add the numeral for this month to the array
         for (let i = 0; i < monthDays; i++) {
             this.dateArray.push({
                 year: year,
@@ -142,6 +153,7 @@ export class Calendar {
                 isThisMonth: true,
                 isToday: false,
                 isSelect: false,
+                hasEvent: (this.isInEvents(year, month, i+1)) ? true : false,
             })
         }
 
@@ -155,7 +167,7 @@ export class Calendar {
             this.dateArray[todayIndex].isToday = true;
         }
 
-       
+        // Add the number of days next month to the array, with some months showing 6 weeks and some months showing 5 weeks
         if (this.dateArray.length % 7 !== 0) {
             let nextMonthAdd = 7 - this.dateArray.length % 7
             for (let i = 0; i < nextMonthAdd; i++) {
@@ -167,6 +179,7 @@ export class Calendar {
                         isThisMonth: false,
                         isToday: false,
                         isSelect: false,
+                        hasEvent: (this.isInEvents(year, 0, i+1)) ? true : false,
                     })
                 } else {
                     this.dateArray.push({
@@ -176,13 +189,15 @@ export class Calendar {
                         isThisMonth: false,
                         isToday: false,
                         isSelect: false,
+                        hasEvent: (this.isInEvents(year, month+1, i+1)) ? true : false,
                     })
                 }
 
             }
         }
 
-        
+        // All date data is now added to the dateArray array
+        // Insert the date data into the new array every seven days
         for (let i = 0; i < this.dateArray.length / 7; i++) {
             for (let j = 0; j < 7; j++) {
                 weekDays.push(this.dateArray[i * 7 + j]);
@@ -193,31 +208,40 @@ export class Calendar {
     }
 
     back() {
-        
+        // Decrementing the year if necessary
         if (this.displayMonth === 0) {
             this.displayYear--;
             this.displayMonth = 11;
         } else {
             this.displayMonth--;
         }
+        this.onMonthSelect.emit({
+          'year': this.displayYear,
+          'month': this.displayMonth
+        });
         this.createMonth(this.displayYear, this.displayMonth);
     }
 
     forward() {
-      
+        // Incrementing the year if necessary
         if (this.displayMonth === 11) {
             this.displayYear++;
             this.displayMonth = 0;
         } else {
             this.displayMonth++;
         }
+        this.onMonthSelect.emit({
+          'year': this.displayYear,
+          'month': this.displayMonth
+        });
         this.createMonth(this.displayYear, this.displayMonth);
     }
 
-   
+    // Select a day, click event
     daySelect(day, i, j) {
-       this.dateArray[this.lastSelect].isSelect = false;
-       
+        // First clear the last click status
+        this.dateArray[this.lastSelect].isSelect = false;
+        // Store this clicked status
         this.lastSelect = i * 7 + j;
         this.dateArray[i * 7 + j].isSelect = true;
 
@@ -225,11 +249,19 @@ export class Calendar {
     }
 }
 
+interface singularDate {
+  year: number,
+  month: number,
+  date: number
+}
+
+// Each grid item of a calendar
 interface dateObj {
     year: number,
     month: number,
-    date: number,
-    isThisMonth: boolean,
+    date: number, // What's the date?
+    isThisMonth: boolean, // Is this the currently selected month?
     isToday?: boolean,
     isSelect?: boolean,
+    hasEvent?: boolean,
 }
